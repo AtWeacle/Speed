4a1f8ad77c5a4e3d1aa641bb9fefeea850d8a78a
Carl Brenner <carl@ceacle.com>
2024-08-25 20:32:19 +0200

    Update constants.ts

--- apps/client/lib/constants.ts
+++ apps/client/lib/constants.ts
@@ -1,1 +1,3 @@
-export const WS_URL = `ws://localhost:${import.meta.env.VITE_WS_PORT || '8080'}`
+export const SERVER_PORT = import.meta.env.VITE_SERVER_PORT || '8080'
+export const SERVER_URL = `http://localhost:${SERVER_PORT}`
+export const WS_URL = `ws://localhost:${SERVER_PORT}`
